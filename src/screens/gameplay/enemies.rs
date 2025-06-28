716a0fb22b64d35d68a4768c3f183b9bd9680217
Sakul <lukasoboy5@gmail.com>
2025-06-28 14:17:42 +0200

    lol

--- src/screens/gameplay/enemies.rs
+++ src/screens/gameplay/enemies.rs
@@ -1,1 +1,21 @@
 
+
+
+
+
+#[repr(usize)]
+#[derive(Clone, Copy, Debug, Eq, PartialEq, Hash)]
+pub enum Type {
+    Flagship,
+    EmpireGoon,
+    PirateShip,
+    Outpoust,
+}
+
+#[derive(Resource)]
+pub struct Ship {
+    pub type: Type,
+    pub position : (f32,f32),
+    pub lifetime : f32,
+    pub weapons :
+}
