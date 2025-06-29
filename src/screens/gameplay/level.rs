aad58d14d8206098ae4f71f63bc83bd642bbd1ee
Theodor Ström <thstro@kth.se>
2025-06-29 14:42:43 +0200

    Working on weapon stuff and also REMOVED MUSIC AGAIN

--- src/screens/gameplay/level.rs
+++ src/screens/gameplay/level.rs
@@ -115,11 +115,6 @@ pub fn spawn_level(
         Visibility::default(),
         StateScoped(Screen::Gameplay),
         children![
-            gen_player(400.0, &player_assets),
-            (
-                Name::new("Gameplay Music"),
-                music(level_assets.music.clone())
-            ),
             gen_planet(
                 &level_assets,
                 &ui_assets,
